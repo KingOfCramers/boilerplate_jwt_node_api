29ce78e592897ca76553ddf0cebba77209dd198e
KingOfCramers <32515581+KingOfCramers@users.noreply.github.com>
2018-06-03 16:34:26 -0400

    fixed ports

--- server/config/portSetter.js
+++ server/config/portSetter.js
@@ -1,6 +1,6 @@
 // SET PORT
 const port = process.env.PORT || 3000;
-const dev = port === 3000 ? "Development" : "Production";
+const dev = port === 3000 ? "development" : "production";
 
 module.exports = {
     dev,
